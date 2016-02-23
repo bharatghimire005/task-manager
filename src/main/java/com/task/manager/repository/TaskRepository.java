9f77efc3ce429d66bc7f95cd244044c75ba56772
navkrishna <navkrishna@tothenew.com>
2016-02-23 12:08:22 +0530

    fixed find all task by name

--- src/main/java/com/task/manager/repository/TaskRepository.java
+++ src/main/java/com/task/manager/repository/TaskRepository.java
@@ -4,11 +4,13 @@ import com.task.manager.model.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by Navkrishna on 21/10/15
  */
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
-    public Task findAllByName(String name);
+    List<Task> findAllByName(String name);
 }
--- src/main/java/com/task/manager/service/TaskService.java
+++ src/main/java/com/task/manager/service/TaskService.java
@@ -29,7 +29,7 @@ public class TaskService {
     }
 
     public List<Task> getTaskByName(String name) {
-        return (List<Task>) taskRepository.findAllByName(name);
+        return taskRepository.findAllByName(name);
     }
 
 }
